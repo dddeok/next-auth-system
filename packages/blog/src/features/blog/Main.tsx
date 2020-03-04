319f3f09af767f5d2372faf67318c801aab07337
dddeok <crew0813@gmail.com>
2020-03-04 16:23:23 +0900

    Yarn Workspaces

--- packages/blog/src/features/blog/Main.tsx
+++ packages/blog/src/features/blog/Main.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import CircleButton from '@dddeok/core/src/components/CircleButton';
+import CircleButton from 'core/src/components/CircleButton';
 import { Button } from 'antd';
 const Container = styled.div`
   width: 1400px;
